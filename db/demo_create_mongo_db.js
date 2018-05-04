a149d51b7e32b8fd54c3006654689f5cafd135fb
Paritosh2018 <paritoshverma2018@gmail.com>
2018-05-04 16:50:15 +0530

    re-initialised db url for local use

--- db/demo_create_mongo_db.js
+++ db/demo_create_mongo_db.js
@@ -1,8 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
-var uri = "mongodb+srv://denakv:a25071982))@cluster0-o1xb4.mongodb.net/test";
+//var url = "mongodb+srv://denakv:a25071982))@cluster0-o1xb4.mongodb.net";
+var url = "mongodb://27017/loginapp";
 
-MongoClient.connect(uri, function(err, db) {
+MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
--- db/demo_mongodb_createcollection.js
+++ db/demo_mongodb_createcollection.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://denakv:a25071982))@cluster0-o1xb4.mongodb.net/";
+//var url = "mongodb+srv://denakv:a25071982))@cluster0-o1xb4.mongodb.net";
+var url = "mongodb://27017/loginapp";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
